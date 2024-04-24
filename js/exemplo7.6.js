e653b47f902daf30d8d689f52121bce19db16b41
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2024-04-23 21:09:44 -0300

    enviando novos exemplos de js

--- js/exemplo7.6.js
+++ js/exemplo7.6.js
@@ -6,27 +6,52 @@ function trocarItem() {
 
     var inPizza = document.getElementById("inPizza");
     var inBebida = document.getElementById("inBebida");
+    var outPedido = document.getElementById("outPedido");
     //se estiver marcado
 
     if(rbPizza.checked) {
 
-        inBebida.className = "oculta"; //oculta o select de bebidas
-        inPizza.className = "exibe"; //exibe select das pizzas
-
+        //inBebida.className = "oculta"; //oculta o select de bebidas
+        //inPizza.className = "exibe"; //exibe select das pizzas
+        var num = inPizza.selectedIndex;
+        var produto = inPizza.options[num].text;
     
     } else {
 
-        inPizza.className = "oculta"; //oculta as pizzas
-        inBebida.className = "exibe"; //exibe as bebidas
+        //inPizza.className = "oculta"; //oculta as pizzas
+        //inBebida.className = "exibe"; //exibe as bebidas
+        var num = inBebida.selectedIndex;
+        var produto = inBebida.options[num].text;
+        var detalhes = inDetalhes.value;
+        itens.push(produto + " (" + detalhes + ")");
+        outPedido.textContent = itens.join("\n");
+
+        limparCampos();
+   
     }
 
 }
 
 //cria a referencia aos radios buttons e associa funcao no evento change
 
-var rbPizza = document.getElementById("rbPizza");
-rbPizza.addEventListener("change", trocarItem);
+//var rbPizza = document.getElementById("rbPizza");
+//rbPizza.addEventListener("change", trocarItem);
 
-var rbBebida = document.getElementById("rbBebida");
-rbBebida.addEventListener("change", trocarItem);
+//var rbBebida = document.getElementById("rbBebida");
+//rbBebida.addEventListener("change", trocarItem);
+
+var btAdicionar = document.getElementById("btAdicionar");
+btAdicionar.addEventListener("click", adicionarItem);
+
+
+function limparCampos() {
+
+    rbPizza.checked = true;
+    inBebida.className = "oculta";
+    inPizza.className = "exibe";
+    inPizza.selectedIndex = 0;
+    inDetalhes.value = "";
+    rbPizza.focus();
+
+}
 
